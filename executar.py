c2f0b77c4f5d3e7d6f2582a7a4241f17b1729b5c
unknown <kelmaria25@gmail.com>
2021-10-09 15:44:34 -0300

    mudando

--- executar.py
+++ executar.py
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-print ("Olá alunas da Turma Construdelas!")
-=======
-print("Olá Mundo!")
-print("Digite o seu nome:")
-nome=input()
-print("O nome digitado foi: "+nome)
->>>>>>> 067ebaa5c07e55d73bb32eec89895feb50f9af2b
+print ("Olá alunas da Turma Construdelas!!!!!")
